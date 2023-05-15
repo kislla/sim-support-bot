696932cfbfe6bdde8fd0766f982b6ca9832dc6de
Edward Kisller <ekisller@liveperson.com>
2023-05-15 19:44:38 +0300

    socket mode on

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     appToken: process.env.SLACK_APP_TOKEN,
+    socketMode:true,
     //processBeforeResponse: true
 });
 
